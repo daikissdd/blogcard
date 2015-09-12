02b2d90b0538f5fd8b8f7f9e18e23cc827a07f97
daikissdd <daikissdd@gmail.com>
2015-09-12 22:20:00 +0900

    first ver complete

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-var _ = require('lodash');
 var blogcard = require('./lib/index');
 var fb = {accessToken: process.env['FB_APP_TOKEN']};
+//var url = 'https://golfes.jp';
+//var url = 'http://jumpin.onl';
+//var url = 'http://thebridge.jp/2015/08/dmm-make-selection';
+//var url = 'https://www.linkedin.com/pub/fdsfa-fsdaf/4/b7a/4b1';
+//var url = 'https://www.linkedin.com/favicon.ico';
+//var url = 'http://www.shiobara-cc.com/';
+//var url = 'https://www.youtube.com/watch?v=CiUkCilM17E&list=FLacYD9DQbJ3IYvz6zj0ONww';
+//var url = 'http://4travel.jp/';
+var url = 'http://news.golfdigest.co.jp/';
 
-blogcard(fb).fatch('https://www.youtube.com/?gl=JP&hl=ja', function(err, res) {
+blogcard(fb).fetch(url, function(err, res) {
 	if (err) return console.log(err);
 	console.log(res);
 });
