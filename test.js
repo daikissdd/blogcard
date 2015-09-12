afca3a63245b3cba471b01e3ee6f73af6299feeb
daikissdd <daikissdd@gmail.com>
2015-09-12 18:12:59 +0900

    update

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var _ = require('lodash');
 var blogcard = require('./lib/index');
-var fb = {accessToken: '1584239041829787|RBKCLOT7O5nE2U4wtJ2tnZTg90o'};
+var fb = {accessToken: process.env['FB_APP_TOKEN']};
 
-blogcard(fb).fatch('http://www.yahoo.co.jp/', function(err, res) {
+blogcard(fb).fatch('https://www.youtube.com/?gl=JP&hl=ja', function(err, res) {
 	if (err) return console.log(err);
 	console.log(res);
 });
